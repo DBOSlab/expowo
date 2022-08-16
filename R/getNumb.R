9152066f2f2e3464e4ba08450ecbe579de83fab2
Débora Zuanny <deborazuanny@gmail.com>
2022-08-16 13:03:46 -0300

    add dot

--- R/getNumb.R
+++ R/getNumb.R
@@ -3,22 +3,22 @@
 getNumb <- function(df,
                     verbose = verbose) {
 
-  # Creating empty lists to save data of interest during all search
+  # Creating empty lists to save data of interest during all search.
   list_spp <- vector("list", length = length(df$powo_uri))
   list_html <- vector("list", length = length(df$powo_uri))
   list_grepl <- vector("list", length = length(df$powo_uri))
 
   for (i in seq_along(df$powo_uri)) {
-    # The tryCatch function helps skipping error in for-loop
+    # The tryCatch function helps skipping error in for-loop.
     tryCatch({
       # Adding a pause 300 seconds of um pause every 500th search,
-      # because POWO website cannot permit constant search
+      # because POWO website cannot permit constant search.
       if (i%%500 == 0) {
         Sys.sleep(300)
       }
 
       list_html[[i]] <- readLines(paste(df$powo_uri[i]), warn = F)
-      # Adding a counter to identify each running search
+      # Adding a counter to identify each running search.
       if (verbose) {
         print(paste0("Searching spp number of... ",
                      df$genus[df$powo_uri == df$powo_uri[i]], " ",
@@ -34,7 +34,7 @@ getNumb <- function(df,
                                                 list_spp[[i]])])
 
       # The function below will print any search error (e.g. site address of a
-      # specific genus is not opening for some reason)
+      # specific genus is not opening for some reason).
     }, error = function(e) {cat(paste("ERROR:",
                                       df$genus[df$powo_uri == df$powo_uri[i]],
                                       df$family[df$powo_uri == df$powo_uri[i]]),
@@ -42,13 +42,13 @@ getNumb <- function(df,
   }
 
   # Filling in with "NA" those genera for which the search failed to open the
-  # POWO site
+  # POWO site.
   temp <- lapply(list_spp, is.null)
   list_spp[unlist(temp)] <- NA
   temp <- lapply(list_spp, function(x) length(x) == 0)
   list_spp[unlist(temp)] <- NA
 
-  # Extracting the number of species from the list during the POWO searching
+  # Extracting the number of species from the list during the POWO searching.
   df$species_number <- unlist(list_spp, use.names = F)
   df$species_number <- as.numeric(df$species_number)
 
