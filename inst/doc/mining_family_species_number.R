70a9546abb864ef4f01f5259fd1f4ca584235bcc
Débora Zuanny <deborazuanny@gmail.com>
2023-02-15 08:33:40 -0300

    gitignore update

---
+++ inst/doc/mining_family_species_number.R
@@ -0,0 +1,41 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----package load-------------------------------------------------------------
+library(expowo)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  powoFam(family = c("Cabombaceae", "Lecythidaceae", "Martyniaceae"),
+#          verbose = TRUE,
+#          save = TRUE,
+#          dir = "results_powoFam/",
+#          filename = "Camb_Lecy_Martyniaceae_diversity")
+
+## ---- echo = FALSE, warning = FALSE-------------------------------------------
+utils::data("angioData")
+utils::data("POWOcodes")
+family <- c("Cabombaceae", "Lecythidaceae", "Martyniaceae")
+fams <- angioData$family[angioData$family %in% family]
+sp_nbr <- as.data.frame(table(fams))
+powo_uri <- gsub("^http", "https", POWOcodes$uri[POWOcodes$family %in% family])
+kew_id <- gsub(".+[:]", "", powo_uri)
+species_number <- sp_nbr$Freq
+res <- data.frame(family, species_number, kew_id, powo_uri)
+
+knitr::kable(res,
+             row.names = FALSE,
+             align = 'c',
+             caption = "TABLE 1. A general `powoFam` search for mining the total species number of three angiosperm families.")
+
+## ---- eval = FALSE------------------------------------------------------------
+#  utils::data(POWOcodes)
+#  
+#  powoFam(POWOcodes$family,
+#          verbose = TRUE,
+#          save = TRUE,
+#          dir = "results_powoFam/",
+#          filename = "all_angiosperms_species_number")
+
---
+++ inst/doc/mining_species_distribution.R
@@ -0,0 +1,40 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----package load-------------------------------------------------------------
+library(expowo)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  powoSpDist(family = c("Begoniaceae", "Lecythidaceae"),
+#             species = c("Hillebrandia sandwicensis", "Lecythis pisonis"),
+#             verbose = TRUE,
+#             save = TRUE,
+#             dir = "results_powoSpDist/",
+#             filename = "Begoniaceae_Lecythidaceae")
+
+## ---- echo = FALSE, warning = FALSE-------------------------------------------
+utils::data("angioData")
+fam <- c("Begoniaceae", "Lecythidaceae")
+df <- angioData[angioData$family %in% fam, ]
+species <- c("Hillebrandia sandwicensis", "Lecythis pisonis")
+df <- df[df$taxon_name %in% species, ]
+
+knitr::kable(df[-c(2, 3, 6, 9, 11, 13)],
+             row.names = FALSE,
+             align = 'c',
+             caption = "TABLE 1. A general `powoSpDist` search for mining 
+             distribution of two angiosperm species.")
+
+## ---- eval = FALSE------------------------------------------------------------
+#  data(POWOcodes)
+#  
+#  powoSpDist(POWOcodes$family,
+#             species = NULL,
+#             verbose = TRUE,
+#             save = TRUE,
+#             dir = "results_powoSpDist/",
+#             filename = "all_angiosperm_distribution")
+
